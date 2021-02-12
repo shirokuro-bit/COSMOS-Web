e29d11938d37fe6e9b5b9b3a5c98370f5a6fce5d
shirokuro <shirokuro.kazu@gmial.com>
2021-02-13 04:29:29 +0900

    2.0.1

--- COSMOS/settings.py
+++ COSMOS/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'COSMOS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'room_management',
+        'USER': 'COSMOS',
+        'PASSWORD': 'PASSWORD',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
