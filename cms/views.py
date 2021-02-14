f76fa2246c05b3f97b41c9a93cf3714380e98333
shirokuro <shirokuro.kazu@gmial.com>
2021-02-14 23:11:42 +0900

    2.2

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    content = {
+        'message': 'こんにちは！Djangoテンプレート！'
+    }
+    return render(request, 'main/index.html', content)
+
+
+def COSMOS(request):
+    return render(request, 'main/COSMOS.html')
